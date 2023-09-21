c5f9c74838b39e7ca385f06532e8241b350de5d0
Sistema2-14 <Sistema2-14@DESKTOP-9OOKO8G>
2023-09-21 11:48:48 -0600

    Agregar archivos de proyecto.

---
+++ OperacionesAritmeticas/Suma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesAritmeticas
+{
+    public class Suma
+    {
+        private int num1;
+        private int num2;
+        private int suma;
+    }
+    
+    
+
+}
